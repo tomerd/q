ea38feb62c461b5f44edf7f82c3b964b8543da32
tomer doron <tomer.doron@gmail.com>
2013-05-05 23:02:00 -0700

    initial commit

--- q/src/core/QlibJava.cpp
+++ q/src/core/QlibJava.cpp
@@ -69,6 +69,7 @@ jstring Java_com_mishlabs_q_Q_native_1post(JNIEnv* env, jobject obj, jlong qp, j
     return env->NewStringUTF(uid.c_str());
 }
 
+// TODO: optimize this
 JNIEXPORT JNICALL
 void Java_com_mishlabs_q_Q_native_1worker(JNIEnv* env, jobject obj, jlong qp, jstring jqueue, jobject jdelegate)
 {
@@ -87,12 +88,13 @@ void Java_com_mishlabs_q_Q_native_1worker(JNIEnv* env, jobject obj, jlong qp, js
         return;
     }
     
+    JavaVM* jvm = NULL;
+    env->GetJavaVM(&jvm);
+    
     const char* queue = env->GetStringUTFChars(jqueue, NULL);
     //((Q*)qp)->worker(queue, [=](Job* job, JobError** error)
     ((Q*)qp)->worker(queue, WorkerDelegate(new std::function<void (const Job*, JobError**)>([=](const Job* job, JobError** error)
     {
-        JavaVM* jvm = NULL;
-        env->GetJavaVM(&jvm);
         jvm->AttachCurrentThread((void**)&env, NULL);
         env->CallVoidMethod(delegate, method, env->NewStringUTF(job->data().c_str()));
         check_for_java_error(env, error);
@@ -101,6 +103,7 @@ void Java_com_mishlabs_q_Q_native_1worker(JNIEnv* env, jobject obj, jlong qp, js
     env->ReleaseStringUTFChars(jqueue, queue);
 }
 
+// TODO: optimize this
 JNIEXPORT JNICALL
 void Java_com_mishlabs_q_Q_native_1observer(JNIEnv* env, jobject obj, jlong qp, jstring jqueue, jobject jdelegate)
 {
@@ -118,13 +121,14 @@ void Java_com_mishlabs_q_Q_native_1observer(JNIEnv* env, jobject obj, jlong qp, 
         env->ThrowNew(env->FindClass("java/lang/Exception"), "could not find method 'perform', make sure the delegate is of correct type");
         return;
     }
+    
+    JavaVM* jvm = NULL;
+    env->GetJavaVM(&jvm);
         
     const char* queue = env->GetStringUTFChars(jqueue, NULL);    
     //((Q*)qp)->observer(queue, [=](Job* job, JobError** error)
     ((Q*)qp)->observer(queue, ObserverDelegate(new std::function<void (const Job*, JobError**)>([=](const Job* job, JobError** error)
     {
-        JavaVM* jvm = NULL;
-        env->GetJavaVM(&jvm);
         jvm->AttachCurrentThread((void**)&env, NULL);        
         env->CallVoidMethod(delegate, method, env->NewStringUTF(job->data().c_str()));
         check_for_java_error(env, error);
